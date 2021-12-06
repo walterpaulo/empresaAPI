18cf38bf39c9c7b8ba6f549a086d0f91a57cc5d8
walterpaulo <walter0paulo@hotmail.com>
2021-12-06 05:22:40 -0300

    init

---
+++ src/main/java/br/com/base/empresaapi/EmpresaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.base.empresaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpresaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/base/empresaapi/EmpresaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.base.empresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
