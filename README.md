42ed93c37be4222d3ec2c8f160b593110f979130
walterpaulo <walter0paulo@hotmail.com>
2021-12-07 05:21:36 -0300

    Nova classe Home no controller

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 
 #### Limpa dependencias
 mvn clean
+ou
+./mvn
 
 
 #### Cria o pacote jar
---
+++ src/main/java/br/com/base/empresaapi/empresaapi/controllers/HomeController.java
@@ -0,0 +1,7 @@
+package br.com.base.empresaapi.empresaapi.controllers;
+
+public class HomeController {
+    public String index(){
+        return "teste";
+    }
+}
