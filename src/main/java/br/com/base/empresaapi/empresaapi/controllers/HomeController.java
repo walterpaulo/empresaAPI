40b97dc038950c6e911a2b6b0515278e69c634bb
walterpaulo <walter0paulo@hotmail.com>
2021-12-07 06:04:30 -0300

    Add CRUD

--- src/main/java/br/com/base/empresaapi/empresaapi/controllers/HomeController.java
+++ src/main/java/br/com/base/empresaapi/empresaapi/controllers/HomeController.java
@@ -1,7 +1,43 @@
 package br.com.base.empresaapi.empresaapi.controllers;
 
+import br.com.base.empresaapi.empresaapi.models.EmpresaModel;
+import br.com.base.empresaapi.empresaapi.models.HomeModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class HomeController {
-    public String index(){
-        return "teste";
+
+    @GetMapping("/empresas")
+    public List<EmpresaModel> index(){
+        List<EmpresaModel> empresa = new ArrayList<>();
+        empresa.add(new EmpresaModel());
+        empresa.add(new EmpresaModel());
+        empresa.add(new EmpresaModel());
+        empresa.add(new EmpresaModel());
+        empresa.add(new EmpresaModel());
+     return empresa;
+    }
+
+    @GetMapping("/empresas/{id}")
+    public EmpresaModel show(@PathVariable int id){
+        EmpresaModel empresa = new EmpresaModel();
+        empresa.setId(1);
+        return empresa;
+    }
+
+    @PostMapping("/empresas")
+    public EmpresaModel create(@RequestBody EmpresaModel empresa){
+        return empresa;
+    }
+    @PutMapping("/empresas/{id}")
+    public EmpresaModel update(@RequestBody EmpresaModel empresa){
+        return empresa;
+    }
+    @DeleteMapping("/empresas/{id}")
+    public EmpresaModel delete(@RequestBody EmpresaModel empresa){
+        return new EmpresaModel();
     }
 }
