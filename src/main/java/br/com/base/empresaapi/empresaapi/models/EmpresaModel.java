7df08716323a0bac2e3c2489a64bbd2ca9d4e71d
walterpaulo <walter0paulo@hotmail.com>
2021-12-07 05:50:06 -0300

    Add Empresa no Model.

--- src/main/java/br/com/base/empresaapi/empresaapi/models/EmpresaModel.java
+++ src/main/java/br/com/base/empresaapi/empresaapi/models/EmpresaModel.java
@@ -1,2 +1,73 @@
-package br.com.base.empresaapi.empresaapi.models;public class EmpresaModel {
+package br.com.base.empresaapi.empresaapi.models;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Timer;
+
+public class EmpresaModel {
+    private int id;
+    private String nome;
+    private String razaoSocial;
+    private String email;
+    private String cnpj;
+    private LocalDateTime dataCriacao = LocalDateTime.now() ;
+    private LocalDateTime dataAtualizacao;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
 }
