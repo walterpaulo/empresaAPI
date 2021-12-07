5e89f0ea85441040500018a3fa7e263c1763eec2
walterpaulo <walter0paulo@hotmail.com>
2021-12-07 06:18:25 -0300

    add camada apresentação.

--- src/main/java/br/com/base/empresaapi/empresaapi/modelviews/HomeModelViews.java
+++ src/main/java/br/com/base/empresaapi/empresaapi/modelviews/HomeModelViews.java
@@ -1,6 +1,6 @@
-package br.com.base.empresaapi.empresaapi.models;
+package br.com.base.empresaapi.empresaapi.modelviews;
 
-public class HomeModel {
+public class HomeModelViews {
     public String getMensagem(){
         return "Bem vindo a API em java feita no desafio 21 dias multipla APIs";
     }
