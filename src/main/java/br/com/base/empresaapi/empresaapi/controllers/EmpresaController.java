cdab4d101809a26e9ce0f93713f66cefc14b3068
walterpaulo <walter0paulo@hotmail.com>
2021-12-08 06:37:13 -0300

    Configurado CRUD.

--- src/main/java/br/com/base/empresaapi/empresaapi/controllers/EmpresaController.java
+++ src/main/java/br/com/base/empresaapi/empresaapi/controllers/EmpresaController.java
@@ -3,8 +3,11 @@ package br.com.base.empresaapi.empresaapi.controllers;
 import br.com.base.empresaapi.empresaapi.models.EmpresaModel;
 import br.com.base.empresaapi.empresaapi.repositorys.EmpresaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,29 +17,45 @@ public class EmpresaController {
     private EmpresaRepo repo;
 
     @GetMapping("/empresas")
-    public List<EmpresaModel> index(){
+    public List<EmpresaModel> index() {
         List<EmpresaModel> empresa = (List<EmpresaModel>) repo.findAll();
-     return empresa;
+        return empresa;
     }
 
     @GetMapping("/empresas/{id}")
-    public EmpresaModel show(@PathVariable int id){
-        EmpresaModel empresa = new EmpresaModel();
-        empresa.setId(1);
-        return empresa;
+    public ResponseEntity<EmpresaModel> show(@PathVariable int id) {
+        if (!repo.existsById(id)) {
+            return ResponseEntity.status(404).build();
+        }
+        EmpresaModel empresa = repo.findById(id).get();
+        return ResponseEntity.ok(empresa);
     }
 
     @PostMapping("/empresas")
-    public EmpresaModel create(@RequestBody EmpresaModel empresa){
-        EmpresaModel obj = repo.save(empresa);
-        return obj;
-    }
-    @PutMapping("/empresas/{id}")
-    public EmpresaModel update(@PathVariable int id,@RequestBody EmpresaModel empresa){
+    public EmpresaModel create(@RequestBody EmpresaModel empresa, HttpServletResponse response) {
+        try {
+            repo.save(empresa);
+        } catch (Exception err) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
         return empresa;
     }
+
+
+    @PutMapping("/empresas/{id}")
+    public ResponseEntity<EmpresaModel> update(@PathVariable int id,@RequestBody EmpresaModel empresa){
+        if (!repo.existsById(id)) {
+            return ResponseEntity.status(404).build();
+        }
+        repo.save(empresa);
+        return ResponseEntity.status(204).build();
+    }
     @DeleteMapping("/empresas/{id}")
-    public EmpresaModel delete(@RequestBody EmpresaModel empresa){
-        return new EmpresaModel();
+    public ResponseEntity<EmpresaModel> delete(@RequestBody EmpresaModel empresa){
+        if (!repo.existsById(empresa.getId())) {
+            return ResponseEntity.status(404).build();
+        }
+        repo.deleteById(empresa.getId());
+        return ResponseEntity.status(204).build();
     }
 }
