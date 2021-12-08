6d861825c3f0e74fb1dff0180c7f68db4adc3b8e
walterpaulo <walter0paulo@hotmail.com>
2021-12-08 05:37:52 -0300

    Configurado conexão com o banco.

--- src/main/java/br/com/base/empresaapi/empresaapi/models/EmpresaModel.java
+++ src/main/java/br/com/base/empresaapi/empresaapi/models/EmpresaModel.java
@@ -1,17 +1,28 @@
 package br.com.base.empresaapi.empresaapi.models;
 
-import java.sql.Time;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Timer;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "empresas")
 public class EmpresaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
+    @Column(name = "rzaoSocial", length = 155, nullable = false)
     private String razaoSocial;
+    @Column(name = "email", length = 200, nullable = false)
     private String email;
+    @Column(name = "cnpj", length = 200, nullable = false)
     private String cnpj;
+    @Column(name = "dataCriacao", nullable = false)
     private LocalDateTime dataCriacao = LocalDateTime.now() ;
+    @Column(name = "dataAtualizacao")
     private LocalDateTime dataAtualizacao;
 
 
